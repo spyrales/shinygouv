a8fd92b921f23f597dc5686dcaee3d6001598206
cervan <cervan@thinkr.fr>
2022-07-20 14:50:48 +0200

    ci: utiliser le bon compte
    Tags : ci
    
    Pourquoi ?
    
    - ne pas se retrouver avec une 401
    
    Quoi ?
    
    - retirer des fichiers inutiles
    - ajouter le compte dans deployApp
    
    Tickets
    
    ticket #

--- dev/deploy.R
+++ dev/deploy.R
@@ -1,9 +1,17 @@
-message("--- Deploy ---\n")
-
 message("--- Add server ---\n")
 
 rsconnect::setAccountInfo(name   = Sys.getenv("SHINYAPPS_NAME"),
                           token  = Sys.getenv("SHINYAPPS_TOKEN"),
-                          secret = Sys.getenv("SHINYAPPS_SECRET"))
+                          secret = Sys.getenv("SHINYAPPS_SECRET")
+)
+
 message("--- Deploying app ---\n")
-rsconnect::deployApp(appDir = ".",appName = "shinygouv-demo")
+appFiles <- list.files(".", recursive = TRUE)
+appFiles <- appFiles[!grepl(".Rprofile|renv|rstudio_|deliverables|dev|data-raw|docker", appFiles)]
+
+rsconnect::deployApp(
+  appDir = ".",
+  account = Sys.getenv("SHINYAPPS_NAME"),
+  appFiles = appFiles,
+  appName = "shinygouv-demo"
+)
