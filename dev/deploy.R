f9b90f00205d53996529fe4d388e3affbb533ce2
cervan <cervan@thinkr.fr>
2022-07-20 15:34:58 +0200

    ci: voir si les var env marchent
    Tags :
    
    Pourquoi ?
    
    - avoir acces aux vars d'env
    
    Quoi ?
    
    - ajouter un print
    
    Tickets
    
    ticket #18

--- dev/deploy.R
+++ dev/deploy.R
@@ -1,5 +1,6 @@
 message("--- Add server ---\n")
 
+message(paste0("-- test print", Sys.getenv("SHINYAPPS_NAME")))
 rsconnect::setAccountInfo(name   = Sys.getenv("SHINYAPPS_NAME"),
                           token  = Sys.getenv("SHINYAPPS_TOKEN"),
                           secret = Sys.getenv("SHINYAPPS_SECRET")
