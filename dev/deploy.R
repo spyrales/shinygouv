a1663c216f9bba7631db56d869a67b767093fb79
colin <colin@thinkr.fr>
2023-06-26 17:35:39 +0200

    deploy: try forcing the install of packages

--- dev/deploy.R
+++ dev/deploy.R
@@ -1,5 +1,20 @@
 message("--- Add server ---\n")
 
+install.packages("remotes")
+remotes::install_deps()
+remotes::install_local()
+
+## Forcing the install of some packages because it
+## doesn't work on the CI for unidentified reasons
+
+remotes::install_cran(
+  c("cpp11", "hms", "progress"),
+  repos = "https://cran.rstudio.com",
+  dependencies = TRUE
+)
+
+message("--- Add server ---\n")
+
 rsconnect::setAccountInfo(
   name = Sys.getenv("SHINYAPPS_NAME"),
   token = Sys.getenv("SHINYAPPS_TOKEN"),
