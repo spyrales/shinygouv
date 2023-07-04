582bac4a162b6fe583f20cb1247e4fd66a70b510
murielle Delmotte <murielle@thinkr.fr>
2023-07-04 14:20:01 +0200

    fix: Renommage appshinydemo
    pourquoi:
    - mieux identifier l app de demo à convertir
    
    quoi:
    - renommage de l'application
    
    Issue #113

--- inst/test.convert.dsfr/dev/01_start.R
+++ inst/test.convert.dsfr/dev/01_start.R
@@ -19,7 +19,7 @@
 ## to change the name in the app_sys() function in app_config.R /!\
 ##
 golem::fill_desc(
-  pkg_name = "appshinydemo", # The Name of the package containing the App
+  pkg_name = "test.convert.dsfr", # The Name of the package containing the App
   pkg_title = "PKG_TITLE", # The Title of the package containing the App
   pkg_description = "PKG_DESC.", # The Description of the package containing the App
   author_first_name = "AUTHOR_FIRST", # Your First Name
